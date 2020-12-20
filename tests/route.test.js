f813966ec9774834cc0991240377acaa16999215
Marian Meres <marian@meres.sk>
2020-12-20 20:51:02 +0100

    test output tweak

--- tests/route.test.js
+++ tests/route.test.js
@@ -6,40 +6,42 @@ const { SimpleRoute } = require('../dist');
 
 const suite = new TestRunner(path.basename(__filename));
 
+const rpad = (s, len = 25) => (s += (' '.repeat(Math.max(0, len - s.length))));
+
 // prettier-ignore
 [
 	// no match
-	['/foo                   ', '/bar            ', null],
-	['/foo                   ', '                ', null],
-	['/foo/[bar]             ', '/foo            ', null],
-	['/[foo]/bar             ', '/foo            ', null],
-	['/[foo]/[bar]           ', '/foo            ', null],
+	['/foo',                     '/bar',              null],
+	['/foo',                     '',                  null],
+	['/foo/[bar]',               '/foo',              null],
+	['/[foo]/bar',               '/foo',              null],
+	['/[foo]/[bar]',             '/foo',              null],
 	// match no params
-	['/                      ', '                ', {}],
-	['                       ', '///             ', {}],
-	['foo                    ', 'foo             ', {}],
-	['//foo///bar.baz/       ', 'foo/bar.baz     ', {}],
-	['foo/bar/baz            ', '//foo//bar/baz//', {}],
-	['#/foo//                ', '#/foo           ', {}],
+	['/',                        '',                  {}],
+	['',                         '///',               {}],
+	['foo',                      'foo',               {}],
+	['//foo///bar.baz/',         'foo/bar.baz ',      {}],
+	['foo/bar/baz',              '//foo//bar/baz//',  {}],
+	['#/foo//',                  '#/foo',             {}],
 	// match with params
-	['/[foo]                 ', '/bar            ', { foo: 'bar' }],
-	['#/foo/[bar]            ', '#/foo/bat       ', { bar: 'bat' }],
-	['#/[foo]/[bar]          ', '#/baz/bat       ', { foo: 'baz', bar: 'bat' }],
-	['#/[foo]/bar            ', '#/baz/bar       ', { foo: 'baz' }],
+	['/[foo]',                   '/bar',              { foo: 'bar' }],
+	['#/foo/[bar]',              '#/foo/bat',         { bar: 'bat' }],
+	['#/[foo]/[bar]',            '#/baz/bat',         { foo: 'baz', bar: 'bat' }],
+	['#/[foo]/bar',              '#/baz/bar',         { foo: 'baz' }],
 	// params with regex constraint
-	['/[id([0-9]+)]          ', '/123            ', { id: '123' }],
-	['/[id(\\d-\\d)]           ', '/1-2            ', { id: '1-2' }],
-	['/[id([0-9]+)]          ', '/foo            ', null],
-	['/foo/[bar]/[id([0-9]+)]', '/foo/baz/123    ', { bar: 'baz', id: '123' }],
-	['/foo/[id([0-9]+)]/[bar]', '/foo/bar/baz    ', null],
+	['/[id([0-9]+)]',            '/123',              { id: '123' }],
+	['/[id(\\d-\\d)]',           '/1-2',              { id: '1-2' }],
+	['/[id([0-9]+)]',            '/foo',              null],
+	['/foo/[bar]/[id([0-9]+)]',  '/foo/baz/123',      { bar: 'baz', id: '123' }],
+	['/foo/[id([0-9]+)]/[bar]',  '/foo/bar/baz',      null],
 	// wrong regex - missing name...
-	['/foo/[([0-9]+)]        ', '/foo/123        ', null],
+	['/foo/[([0-9]+)]',          '/foo/123',          null],
 ]
 	.forEach(([route, match, expected]) => {
 		suite.test(
-			`${route} -> ${match} => ${JSON.stringify(expected)}`,
+			`${rpad(route, 23)} -> ${rpad(match, 16)} => ${JSON.stringify(expected)}`,
 			() => {
-				let actual = new SimpleRoute(route).parse(match);
+				const actual = new SimpleRoute(route).parse(match);
 				assert(_.isEqual(actual, expected), JSON.stringify(actual));
 			}
 		)
